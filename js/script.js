f1873d8d2eb0037c66744ff9256b46ea085a9853
Andrea de Brito <andreadebrito@gmail.com>
2020-08-26 17:00:42 -0300

    Update 26-08 17h00

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 // Get DOM Elements
-const modal = document.querySelector('#my-modal');
-const modalBtn = document.querySelector('#modal-btn');
-const closeBtn = document.querySelector('.close');
+const modal2 = document.querySelector('#my-modal');
+const modalBtn2 = document.querySelector('#modal-btn');
+const closeBtn2 = document.querySelector('.close');
 
 // Events
-modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
+modalBtn2.addEventListener('click', openModal);
+closeBtn2.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick);
 
 // Open
 function openModal() {
-  modal.style.display = 'block';
+  moda2l.style.display = 'block';
 }
 
 // Close
 function closeModal() {
-  modal.style.display = 'none';
+  modal2.style.display = 'none';
 }
 
 // Close If Outside Click
 function outsideClick(e) {
   if (e.target == modal) {
-    modal.style.display = 'none';
+    modal2.style.display = 'none';
   }
 }
